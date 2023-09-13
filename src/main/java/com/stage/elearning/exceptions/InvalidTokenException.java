992beccef9f8f3e1c5e57b40faa78cae21c8d22f
Mohamed El Mahdi Trabelsi <62462645+DevMahdiTR@users.noreply.github.com>
2023-09-14 00:05:57 +0100

    Delete src/main directory

--- src/main/java/com/stage/elearning/exceptions/InvalidTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.stage.elearning.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class InvalidTokenException extends  RuntimeException{
-    public InvalidTokenException(String message) { super(message);}
-}
--- src/main/java/com/stage/elearning/service/email/EmailSenderService.java
+++
@@ -1,7 +0,0 @@
-package com.stage.elearning.service.email;
-
-import org.jetbrains.annotations.Contract;
-
-public interface EmailSenderService {
-    public void sendEmail(final String toEmail ,final String userName , final String link);
-}
