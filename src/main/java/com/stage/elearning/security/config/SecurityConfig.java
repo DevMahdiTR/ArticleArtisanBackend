1e4f3248d58576051abe3a3d7255ba62f3470f0d
Mohamed El Mahdi Trabelsi <trabelsimahdi79@gmail.com>
2023-09-26 12:21:04 +0100

    fix cross origin

--- src/main/java/com/stage/elearning/security/config/SecurityConfig.java
+++ src/main/java/com/stage/elearning/security/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://ruspina.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
